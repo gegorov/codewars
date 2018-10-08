b4ced85c5c4a40ba420b56dd465a3da700b23aea
gegorov <gegorov@yahoo.com>
2018-10-08 23:43:56 +0300

    feat: add opposite number solution

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,12 @@
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+// But can you do it in 1 line of code and without any conditionals?
+
+const opposite = number => -1 * number;
+
+console.log(opposite(-1));
